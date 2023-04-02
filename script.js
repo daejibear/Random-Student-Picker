d822dcd8de974e4e305337b130847f8346ca5a8b
daejibear <118245676+daejibear@users.noreply.github.com>
2023-04-02 19:35:22 +0900

    generate random student function and display completed

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let studentsValue = document.querySelector(".students-value")
 let submitStudentValue = document.querySelector(".submit-button")
+let numberOfStudentsEl = document.querySelector(".numbers-display")
+let generateButton = document.querySelector(".generate-button")
 
 let numberOfStudents = null;
 
 submitStudentValue.onclick = () => {
     numberOfStudents = studentsValue.value;
+    numberOfStudentsEl.textContent = numberOfStudents;
 console.log(numberOfStudents);}
+
+
+generateButton.onclick = () => {
+    let randomStudents = Math.floor(Math.random()*numberOfStudents+1);
+    numberOfStudentsEl.textContent = randomStudents;
+
+}
